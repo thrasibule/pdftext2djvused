f4944095bb0114812b08d22367b39b525ac7a9f8
Guillaume Horel <guillaume.horel@gmail.com>
2011-10-02 14:39:05 -0400

    Handle non ASCII characters

--- pdftext2djvused.py
+++ pdftext2djvused.py
@@ -14,4 +14,7 @@ for page in document.findall('.//{{{0}}}page'.format(ns)):
         djvuxmax = float(word.attrib['xMax']) * dpi /72
         djvuymin = (float(page.attrib['height']) - float(word.attrib['yMax'])) * dpi /72
         djvuymax = (float(page.attrib['height']) - float(word.attrib['yMin'])) * dpi /72
-        print '( word {0} {1} {2} {3} \"{4}\")'.format(int(djvuxmin), int(djvuymin), int(djvuxmax), int(djvuymax), word.text)
+        octalescapedtext = ''.join(["\{0:o}".format(c) if c>127 else chr(c) for c in map(ord,word.text)])
+        wordexpr = '( word {0} {1} {2} {3} \"{4}\")'.format(int(djvuxmin), int(djvuymin), int(djvuxmax), int(djvuymax), octalescapedtext)
+        print wordexpr
+    print ")"
