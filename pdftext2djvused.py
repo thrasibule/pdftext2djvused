a53999a4b66950491be9fc1c4f24102892ca34ad
Guillaume Horel <guillaume.horel@gmail.com>
2011-10-02 17:28:13 -0400

    Fix the octal encoding for utf8 string
    also escape quote character (thanks Iva!)

--- pdftext2djvused.py
+++ pdftext2djvused.py
@@ -8,13 +8,15 @@ dpi = 300
 for page in document.findall('.//{{{0}}}page'.format(ns)):
     djvuheight = float(page.attrib['height']) * dpi / 72
     djvuwidth = float(page.attrib['width']) * dpi / 72
-    print '( page 0 0 {0} {1}'.format(int(djvuwidth), int(djvuheight))
+    print '(page 0 0 {0} {1}'.format(int(djvuwidth), int(djvuheight))
     for word in page.getchildren():
         djvuxmin = float(word.attrib['xMin']) * dpi /72
         djvuxmax = float(word.attrib['xMax']) * dpi /72
         djvuymin = (float(page.attrib['height']) - float(word.attrib['yMax'])) * dpi /72
         djvuymax = (float(page.attrib['height']) - float(word.attrib['yMin'])) * dpi /72
-        octalescapedtext = ''.join(["\{0:o}".format(c) if c>127 else chr(c) for c in map(ord,word.text)])
-        wordexpr = '( word {0} {1} {2} {3} \"{4}\")'.format(int(djvuxmin), int(djvuymin), int(djvuxmax), int(djvuymax), octalescapedtext)
+        octalescapedtext = ''.join(["\{0:o}".format(c) if c>127 else chr(c) for c in map(ord,word.text.encode('utf8'))])
+        #escape quote character
+        octalescapedtext = octalescapedtext.replace('"','\\"')
+        wordexpr = '(word {0} {1} {2} {3} \"{4}\")'.format(int(djvuxmin), int(djvuymin), int(djvuxmax), int(djvuymax), octalescapedtext)
         print wordexpr
     print ")"
